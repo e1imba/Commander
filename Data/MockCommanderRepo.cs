0bca2b98f20d0b0c739dc321506c9b89748f43b0
Ivan Arsenović <ivanarsenovic015@gmail.com>
2020-08-09 21:02:05 +0200

    Add project files.

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,35 @@
+﻿using Commander.Models;
+using System.Collections.Generic;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+                {
+                new Command { Id = 0, HowTo = "Kako", Line = "La Linea", Platform = "Zvezdica" },
+                new Command { Id = 1, HowTo = "Onako", Line = "Šema", Platform = "Ivan" },
+                new Command { Id = 2, HowTo = "Radi", Line = "Ne pada", Platform = "Ne raste" }
+                };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Kako", Line = "La Linea", Platform = "Zvezdica" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,16 @@
+﻿using Commander.Dtos;
+using Commander.Models;
+using AutoMapper;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+        }
+    }
+}
